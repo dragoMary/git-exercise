15c38612857cd4acf1d5ca66eb635dd5bd01eb05
mariana <dragomir.mariana@web.de>
2022-12-20 13:00:08 +0100

    change2

---
+++ main.py
@@ -0,0 +1,53 @@
+x = 3
+
+y = 2
+print(x + y)
+print(x * y)
+print(x % y) #gives the rest
+print(x / y)
+print(x // y) #gives the rest of calculation
+x = 7
+y = 3
+#print(x ** y)
+c = x * x * x
+
+# print(max(1765, 1.987987, 1, 10, 9))
+min(1, 10, 123654)
+# print(max('A', 'a', 'B'))
+# absolute value
+print(abs(-2))
+print(pow(2, 3)) #power
+print(2 ** 3)
+
+print(round(1.56))
+
+print(round(1.567845328, 3)) #show the next 3 ziffern
+
+
+
+# x = math.sqrt(49)
+print(x)
+
+x = math.ceil(2.34)
+print(x)
+
+x = math.floor(2.34)
+print(x)
+
+
+
+x = 5 # change the value of x, which will be 1 instead 5 at the end
+y = 3
+z = 1
+x = x - y - z
+# x -= y
+print(x)
+
+
+
+import EX
+x = math.sqrt(1)
+print(x)
+
+
+
